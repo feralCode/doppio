930d064c0afe8189834841d4ca51b53c234cd13e
Brandon Moody <brandon@carematics.com>
2014-09-30 00:28:33 -0400

    ver 0.0.1

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #test python tornado script
 
-from datetime import date
+from datetime import datetime
 import logging
 import tornado.escape
 import tornado.ioloop
@@ -15,16 +15,12 @@ from tornado.options import define, options, parse_command_line
 define("port", default=8001, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode")
 
-class MessageBuffer():
-  waiters = set()
-  cache = []
-  cache_size = 200
 
 #create cache for all rooms
-__terminus_message_buffer__ = MessageBuffer()
-__trill_message_buffer__    = MessageBuffer()
-__random_message_buffer__   = MessageBuffer()
-__alt_message_buffer__      = MessageBuffer()
+#__terminus_message_buffer__ = MessageBuffer()
+#__trill_message_buffer__    = MessageBuffer()
+#__random_message_buffer__   = MessageBuffer()
+#__alt_message_buffer__      = MessageBuffer()
 
 # class BaseHandler(tornado.websocket.WebSocketHandler):
 #   #lets allow cross origin by default
@@ -67,25 +63,39 @@ __alt_message_buffer__      = MessageBuffer()
 #       r = tornado.escape.json_decode(user_json)
 #     return r
 
+class MainHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.render("www/index.html")
+
 
 class ChatHandler(tornado.websocket.WebSocketHandler):
   waiters = set()
-  cache = []
+  message_cache = []
+  whispers_cache = []
   cache_size = 200
 
   def open(self):
     logging.info("on websocket open")
+    self.id = uuid.uuid4() #random id for user
+    self.name = '';
     ChatHandler.waiters.add(self)
+    self.write_message(tornado.escape.json_encode(ChatHandler.message_cache))
 
   def on_close(self):
     logging.info("on websocket closed")
     ChatHandler.waiters.remove(self)
 
   @classmethod
-  def update_cache(cls, chat):
-    cls.cache.append(chat)
-    if len(cls.cache) > cls.cache_size:
-      cls.cache = cls.cache[-cls.cache_size:]
+  def update_message_cache(cls, chat):
+    cls.message_cache.append(chat)
+    if len(cls.message_cache) > cls.cache_size:
+      cls.message_cache = cls.message_cache[-cls.cache_size:]
+
+  @classmethod
+  def update_whisper_cache(cls, chat):
+    cls.whispers_cache.append(chat)
+    if len(cls.whispers_cache) > cls.cache_size:
+      cls.whispers_cache = cls.whispers_cache[-cls.cache_size:]
 
   @classmethod
   def send_updates(cls, chat):
@@ -96,18 +106,51 @@ class ChatHandler(tornado.websocket.WebSocketHandler):
       except:
         logging.error("Error sending message", exc_info=True)
 
+  @classmethod
+  def send_whisper(cls, chat):
+    logging.info("sending whisper to %s", chat['to_name'])
+    for waiter in cls.waiters:
+      if(waiter.id == chat.from_id): #send whisper to appropriate user
+        try:
+          waiter.write_message(chat)
+        except:
+          logging.error("Error sending message", exc_info=True)
+
   def on_message(self, message):
-    logging.info("got message %r", message)
+    logging.info("got message %r from %s", message, self.name,)
     parsed = tornado.escape.json_decode(message)
-    chat = {
-      "id": str(uuid.uuid4()),
-      "from": parsed["name"],
-      "body": parsed["body"],
-      "room": parsed["room"],
-      "date": date.today().isoformat()
-      }
-    ChatHandler.update_cache(chat)
-    ChatHandler.send_updates(chat)
+
+    #if name is set from remote client update
+    if("name" in parsed):
+        self.name = parsed['name'];
+
+    #name and room required to post
+    elif(self.name != '' and 'room' in parsed):
+      chat = {
+        "id"        : str(uuid.uuid4()),
+        "from_id"   : str(self.id),
+        "from_name" : str(self.name),
+        "body"      : parsed["body"],
+        "date"      : int(datetime.now().strftime("%s")) * 1000 
+        }
+      ChatHandler.update_message_cache(chat)
+      ChatHandler.send_updates(chat)
+
+    #to_name, to_id and room required to post
+    elif(self.name != '' and 'to_id' in parsed and 'to_name' in parsed):
+      chat = {
+        "id"        : str(uuid.uuid4()),
+        "from_id"   : str(self.id),
+        "from_name" : str(self.name),
+        "to_name"   : parsed["to_name"],
+        "to_id"     : parsed["to_id"], 
+        "body"      : parsed["body"],
+        "room"      : parsed["room"],
+        "date"      : int(datetime.now().strftime("%s")) * 1000 
+        }
+
+      ChatHandler.update_whisper_cache(chat)
+      ChatHandler.send_whisper(chat)
 
 class VersionHandler(tornado.web.RequestHandler):
   def get(self):
@@ -118,11 +161,11 @@ def main():
   parse_command_line()
   app = tornado.web.Application(
     [
-      (r"/random", ChatHandler),
-      (r"/trill", ChatHandler),
+      (r"/atlanta", ChatHandler),
       (r"/version", VersionHandler),
+      (r"/", MainHandler),
       (r"/(.*)", tornado.web.StaticFileHandler, {"path": "www"})
-    ],
+          ],
     static_path=os.path.join(os.path.dirname(__file__), "www"),
     debug=options.debug,
     )
--- www/js/my-app.js
+++ www/js/my-app.js
@@ -10,6 +10,7 @@ var mainView = myApp.addView('.view-main', {
   dynamicNavbar: true
 });
 
+var message_cache = [];
 /*
 var setCerdentials = function(sessionObj) {
   // set document cookie from the passed session for future requests
@@ -21,51 +22,151 @@ var setCerdentials = function(sessionObj) {
   document.cookie = cookie;
 };
 */
+function generator() {
+  // Add your own words to the wordlist. Be carefull to obey the showed syntax
 
-var room = '';
+  var wordlist1 = ["Cool","Masked","Bloody","Lame","Big","Stupid","Drunk","Rotten",
+        "Blue","Black","White","Red","Purple","Golden","Silver","Sexy","Slick",
+        "Trappin","Gucci","Flocka", "Trill", "Fashion", "Magic", "Guessin"];
+
+  var wordlist2 = ["Hamster","Moose","Lama","Duck","Bear","Eagle","Tiger",
+        "Rocket","Bullet","Knee","Foot","Hand","Mistress","Geisha","Flame",
+        "Squad", "Bish", "jong un"];
+
+  // Random numbers are made 
+  var randomNumber1 = parseInt(Math.random() * wordlist1.length);
+  var randomNumber2 = parseInt(Math.random() * wordlist2.length);
+  var name = wordlist1[randomNumber1] + " " + wordlist2[randomNumber2];     
+
+  return name;
+}  
+
+//////////////////////////////////////////////////////////////////////////////
+//messages.html helpers
+///////////////////////////////////////////////////////////////////////////////
+function messengerHelper(context) {
+  //return if no attachments found
+  if (!context || context.length < 1) {
+    return '';
+  }
+  //html string to be returned
+  var htmlRt = '';
+  //used to measure time in between messages
+  var lastMsgDate  = 0;
+  var todaysDate   = moment().hour(0).minute(0).valueOf();
+  var yesterDate   = moment().day(-1).hour(0).minute(0).valueOf();
+  var lastUser     = '';
+  //sort messages by date for processing
+  var sortedByTime = _.sortBy(context, function(msg) {
+    //might be faster way? maybe on ajax
+    return msg.date;
+  });
+
+  //lets get a count of all the stat status'
+  _.each(sortedByTime,function(v){
+    var stampStr = '';
+    
+    console.log('message content ' + v.body);
+
+    if(v.date > todaysDate){
+      stampStr = 'Today ';
+    }
+    else if (v.date > yesterDate){
+      stampStr = 'Yesterday ';
+    }
+    else {
+      stampStr = moment(v.date).format('dddd');
+    }
+    //if there is one minute difference between messages 
+    //print time stamp
+    if(moment(v.date).diff(moment(lastMsgDate)) > 60000) {
+      htmlRt += '<div class="messages-date">' +
+        stampStr + ' ' +
+        '<span>' +
+          moment(v.date).format('h:mm A') +
+        '</span>' +
+      '</div>';
+    }
+
+    htmlRt += '<div class="message message-received">' +
+      '<div class="message-name">' + v.from_name + '</div>' +
+      '<div class="message-text">' +
+        v.body +
+      '</div>' +
+    '</div>';
+
+    //set time to last message timestamp
+    lastMsgDate = v.date;
+    //set last user to last userid see
+    lastUser    = v.from_id;
+  });
+  $$(".page-content.messages-content .messages").html(htmlRt);
+  myApp.updateMessagesAngles($$(".messages"));
+}
+
 var updater = {
     socket: null,
 
-    start: function(room) {
-        var url = "ws://" + location.host + "/" + room;
+    start: function() {
+        var url = "ws://" + location.host + "/atlanta" ;
         updater.socket = new WebSocket(url);
         updater.socket.onmessage = function(event) {
             updater.showMessage(JSON.parse(event.data));
         };
+        updater.socket.onopen = function() {
+            //set user name on server
+            updater.socket.send(JSON.stringify({ name: generator() }));
+        };
     },
     showMessage: function(message) {
         //var existing = $("#m" + message.id);
-        //if (existing.length > 0) return;
+        if (message.length === 0) return;
 
-        // Add Message
-        myApp.addMessage({
-            text: message.body,
-            type: 'received',
-            day: 'Today' || false,
-            time: message.date || false
-        });
+        console.log('received message');
+        console.log(message);
+
+        if ('body' in  message) {
+          message_cache.push(message);
+          // Add Message
+          myApp.addMessage({
+              name: message.from_name,
+              text: message.body,
+              type: 'received',
+              day: 'Today' || false,
+              time: moment(message.date).format('h:mm A') || false
+          });
+        }
+
+        if (message.length > 1) {
+          _.each(message, function(v) {
+            message_cache.push(v);
+            messengerHelper(message_cache);
+            /*myApp.addMessage({
+              name: v.from_name,
+              text: v.body,
+              type: 'received',
+              day: 'Today' || false,
+              time: moment(message.date).format('h:mm A') || false
+            });*/
+
+          });//end of each
+        }
     }
   };//end of updater
 
 /* ===== Document load Actions ===== */
 window.onload = function() {
   console.log('document load compelte');
+  updater.start();
+
+  var namePopup = $$('.name-popup');
+  myApp.popup(namePopup);
+
+
+
+  messengerHelper(message_cache);
   
-  
-};
 
-$$(document).on('click', '.room-link', function() {
-  room = $$(this).attr('data-room');
-  updater.start(room); //start websocket listener
-});
-$$(document).on('click', '.back.link', function() {
-
-  updater.socket.close(); //start websocket listener
-});
-
-
-/* ===== Messages Page ===== */
-myApp.onPageInit('messages', function (page) {
   $$('.messagebar textarea').on('keyup', function (e) {
     console.log(e.keyCode);
     if (e.keyCode == 13) {
@@ -73,18 +174,18 @@ myApp.onPageInit('messages', function (page) {
     }
   });
 
-  $$('.messagebar a.link').on('click', function () {
+  $$('.messagebar a.link').on('click touchstart', function () {
+    var room = 'atlanta';
     var textarea = $$('.messagebar textarea');
     var messageText = textarea.val();
     if (messageText.length === 0) return;
-    var message = {
+    var data = {
       body: messageText,
-      room: room,
-      name: 'anonymous'
+      room: room
     };
-    updater.socket.send(JSON.stringify(message));
+    updater.socket.send(JSON.stringify(data));
     // Empty textarea
     textarea.val('').trigger('change').focus();
   });
 
-});
+};
